d864c35a6e51f15e2a0c89ff36312930ad984aed
DontDDoS <git@dontddos.me>
2026-02-28 23:49:11 +0000

    Add HTML generation, template, and debug flag
    Introduce HTML generation from YAML configs: add src/generators.py (generate_body) and a skeleton template (templates/skeleton.html). Update src/main.py to load the template, read theme CSS, build body from config files, and emit HTML per YAML; also add debug logging, OUTPUT_DIR handling, and export filename logic. Add DEBUG_OUTPUT environment in compose.yaml and updated config/index.yaml for testing the new changes.

---
+++ src/generators.py
@@ -0,0 +1,34 @@
+def generate_body(file, debug_output):
+    if debug_output:
+        print(f"Generating body from {file}")
+    content = file["content"]
+    if debug_output:
+        print(f"Content:\n{content}")
+
+    all_section_html = ""
+    for section_name, section_content in content.items():
+        section_html = f"""<div class="section" id="section-name-{section_name}">
+BOXES
+</div>"""
+        if debug_output:
+            print(f"current section: {section_name}")
+            print(f"section content: {section_content}")
+        all_box_html = ""
+        for box_name, box_content in section_content.items():
+            box_title = box_content["title"]
+            if debug_output:
+                print(f"  box: {box_name}")
+                print(f"  box content: {box_content}")
+            box_html = f"""<div class="box" id="box-name-{box_name}">
+<span class="box-title">{box_title}</span>
+</div>"""
+            all_box_html += box_html
+        if debug_output:
+            print(f"all box html:\n{all_box_html}")
+        section_html = section_html.replace("BOXES", all_box_html)
+        if debug_output:
+            print(f"section html:\n{section_html}")
+        all_section_html += section_html
+    if debug_output:
+        print(f"all section html\n{all_section_html}")
+    return all_section_html
--- src/main.py
+++ src/main.py
@@ -2,8 +2,18 @@ import os
 import time
 
 from utils import *
+from generators import *
 
 def main():
+    skeleton_template = read_file("/app/templates/skeleton.html")
+
+    # Get debug output
+    try:
+        debug_output = bool(grab_env("DEBUG_OUTPUT", False))
+    except TypeError:
+        print("ERROR: DEBUG_OUTPUT could not be converted to an boolean.")
+        hang()
+
     # Get checking interval
     try:
         check_interval = int(grab_env("CHECK_INTERVAL", 10))
@@ -11,21 +21,62 @@ def main():
         print("ERROR: CHECK_INTERVAL could not be converted to an integer.")
         hang()
 
+    # Get export path
+    export_path = grab_env("OUTPUT_DIR", "/output/")
+    if export_path[-1] != "/":
+        export_path += "/"
+
     # Get config directory
     config_directory = grab_env("CONFIG_DIR", "/config/")
-    
     if config_directory[-1] != "/":
         config_directory += "/"
 
     # Set theme
     theme = grab_env("THEME", "catppuccin-mocha")
 
+    if debug_output:
+        print("Starting with the following env variables:")
+        print(f" | DEBUG_OUTPUT: {debug_output}")
+        print(f" | CHECK_INTERVAL: {check_interval}")
+        print(f" | OUTPUT_DIR: {export_path}")
+        print(f" | CONFIG_DIR: {config_directory}")
+        print(f" | THEME: {theme}")
+
     while True:
         files = os.listdir(config_directory)
+        if debug_output:
+            print(f"files: {files}")
 
         for file in files:
+            if debug_output:
+                print(f"looping over {files}")
+                print(f" | current file: {file}")
             if file.split(".")[-1] == "yaml":
-                print(read_yaml(config_directory + file))
+                content = read_yaml(config_directory + file)
+                if debug_output:
+                    print(f"  | {file} is a yaml file")
+                    print(f"  | Content: \n{content}")
+
+                export_file_name = file + ".html"
+
+                print(f"Generating new HTML from {file} in {export_path}{export_file_name}")
+
+                html = skeleton_template.replace("TITLE", content["title"])
+                
+                theme_variables = read_file(f"/app/themes/{content["theme"]}.css")
+                html = html.replace("THEME_VARIABLES", theme_variables)
+
+                if debug_output:
+                    print(f"  | Generated HTML so far:\n{html}")
+
+                body = generate_body(content, debug_output)
+
+                html = html.replace("BODY", body)
+                
+                if debug_output:
+                    print(f"  | Generated HTML so far:\n{html}")
+
+
 
         print(f"DONE | sleeping for {check_interval} seconds")
         time.sleep(check_interval)
